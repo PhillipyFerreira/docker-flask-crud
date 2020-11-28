567146daa30ca660fb5e8de020ff11ec63eb922b
Pavan Aleti <pavan.hpsa@gmail.com>
2020-11-28 17:22:52 +0000

    latest files

--- app/module/database.py
+++ app/module/database.py
@@ -1,12 +1,13 @@
 '''
-Created on Jan 10, 2017
+Created on Sep 10, 2017
 
-@author: hanif
+@author: Pavan Aleti
 '''
 
 import pymysql
 
 class Database:
+    #adicionar métricas de latência e tracing
     def connect(self):
         return pymysql.connect("phonebook-mysql","dev","dev","crud_flask" )
 
@@ -19,9 +20,10 @@ class Database:
                 cursor.execute("SELECT * FROM phone_book order by name asc")
             else:
                 cursor.execute("SELECT * FROM phone_book where id = %s order by name asc", (id,))
-
+            #Four Golden signal: Medir latência das consultas SELECT
             return cursor.fetchall()
         except:
+            #logging ERRO para registrar erros de consulta
             return ()
         finally:
             con.close()
@@ -34,10 +36,12 @@ class Database:
             cursor.execute("INSERT INTO phone_book(name,phone,address) VALUES(%s, %s, %s)", (data['name'],data['phone'],data['address'],))
             con.commit()
 
+            #logging INFO para registrar inserções bem-sucedidas
+
             return True
         except:
             con.rollback()
-
+            #logging ERROR para registrar falhas na inserção
             return False
         finally:
             con.close()
@@ -49,11 +53,11 @@ class Database:
         try:
             cursor.execute("UPDATE phone_book set name = %s, phone = %s, address = %s where id = %s", (data['name'],data['phone'],data['address'],id,))
             con.commit()
-
+            #logging oara registrar atualizações bem-sucedidas
             return True
         except:
             con.rollback()
-
+            #logging ERROR para registrar falhas na inserção
             return False
         finally:
             con.close()
@@ -65,11 +69,12 @@ class Database:
         try:
             cursor.execute("DELETE FROM phone_book where id = %s", (id,))
             con.commit()
+            #logging INFO para registrar exclusões bem-sucedidas
 
             return True
         except:
             con.rollback()
-
+            #logging ERROR para registrar falhas na exclusão
             return False
         finally:
             con.close()
