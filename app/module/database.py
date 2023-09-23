a773ef6fb5d24bd20027be218096d584cba50f30
Guilherme Cerqueira Neto <34224273+guilhermecneto@users.noreply.github.com>
2023-09-23 16:44:54 -0300

    Update database.py

--- app/module/database.py
+++ app/module/database.py
@@ -6,6 +6,7 @@ Created on Sep 10, 2017
 
 import pymysql
 
+# Adicionar LOG
 class Database:
     def connect(self):
         return pymysql.connect("phonebook-mysql","dev","dev","crud_flask" )
@@ -25,7 +26,7 @@ class Database:
             return ()
         finally:
             con.close()
-
+#Adicionar LOG
     def insert(self,data):
         con = Database.connect(self)
         cursor = con.cursor()
@@ -41,7 +42,7 @@ class Database:
             return False
         finally:
             con.close()
-
+#Adicionar LOG
     def update(self, id, data):
         con = Database.connect(self)
         cursor = con.cursor()
@@ -57,7 +58,7 @@ class Database:
             return False
         finally:
             con.close()
-
+#Adicionar LOG
     def delete(self, id):
         con = Database.connect(self)
         cursor = con.cursor()
