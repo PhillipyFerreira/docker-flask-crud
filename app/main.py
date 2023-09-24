ace517b3a8bdd66f250d71d3774231cae8191a48
anaclarasempreceub <145225870+anaclarasempreceub@users.noreply.github.com>
2023-09-24 16:56:17 -0300

    Update main.py
    Inclusão de logging em fonte.

--- app/main.py
+++ app/main.py
@@ -26,72 +26,90 @@ def add():
     return render_template('add.html')
 
 @app.route('/addphone', methods = ['POST', 'GET'])
+# Inclusão de logging de INFO.
 def addphone():
     if request.method == 'POST' and request.form['save']:
         if db.insert(request.form):
             flash("A new phone number has been added")
         else:
+            # Inclusão de logging de ERROR.
             flash("A new phone number can not be added")
-
+            
+        # Inclusão de logging de INFO.
         return redirect(url_for('index'))
     else:
         return redirect(url_for('index'))
 
+# Inclusão de logging de INFO.
 @app.route('/update/<int:id>/')
 def update(id):
     data = db.read(id);
 
+    # Inclusão de logging de INFO.
     if len(data) == 0:
         return redirect(url_for('index'))
     else:
+        # Inclusão de logging de INFO.
         session['update'] = id
         return render_template('update.html', data = data)
 
 @app.route('/updatephone', methods = ['POST'])
 def updatephone():
+    # Inclusão de logging de INFO.
     if request.method == 'POST' and request.form['update']:
 
         if db.update(session['update'], request.form):
             flash('A phone number has been updated')
-
+            
+        # Inclusão de logging de ERROR.   
         else:
             flash('A phone number can not be updated')
 
         session.pop('update', None)
 
+        # Inclusão de logging de INFO.
         return redirect(url_for('index'))
     else:
+        # Inclusão de logging de INFO.
         return redirect(url_for('index'))
 
 @app.route('/delete/<int:id>/')
 def delete(id):
     data = db.read(id);
 
+    # Inclusão de logging de INFO.
     if len(data) == 0:
         return redirect(url_for('index'))
     else:
+        # Inclusão de logging de INFO.
         session['delete'] = id
         return render_template('delete.html', data = data)
 
 @app.route('/deletephone', methods = ['POST'])
 def deletephone():
+    # Inclusão de logging de INFO.
     if request.method == 'POST' and request.form['delete']:
 
         if db.delete(session['delete']):
             flash('A phone number has been deleted')
 
+        # Inclusão de logging de ERROR.
         else:
             flash('A phone number can not be deleted')
 
         session.pop('delete', None)
 
+        # Inclusão de logging de INFO.
         return redirect(url_for('index'))
     else:
+        # Inclusão de logging de INFO.
         return redirect(url_for('index'))
 
 @app.errorhandler(404)
 def page_not_found(error):
+    # Inclusão de logging de WARNING.
     return render_template('error.html')
 
 if __name__ == '__main__':
+    # Inclusão de logging de INFO.
     app.run(port=5000, host="0.0.0.0")
